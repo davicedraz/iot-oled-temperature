f99d77fcaeadd2911390f61c3d367a849f330935
davi_cedraz <davioler@gmail.com>
2019-04-13 04:44:34 -0300

    Implements mqqt publish and connection to Dojot platform

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include "DHT.h"
 #include <Wire.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
 #include "SSD1306Wire.h"
 
 #define LED D4
@@ -7,16 +9,61 @@
 DHT dht;
 SSD1306Wire display(0x3c, D2, D3);
 
+WiFiClient client;
+PubSubClient mqqtClient(client);
+
+const char* SSID_WIFI = "";
+const char* PASSWORD_WIFI = "";
+const char* DOJOT_URL = "";
+const char* TOPIC_PUBLISH = "";
+
 void setup() {
   dht.setup(D1);
   display.init();
-  
+  Serial.begin(9600);
+
   pinMode(LED, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  mqqtClient.setServer(DOJOT_URL, 1883);
+}
+
+void wirelessConnection() {
+  if (WiFi.status() == WL_CONNECTED){
+    digitalWrite(LED_BUILTIN, LOW);
+    return;
+  }
+
+  WiFi.begin(SSID_WIFI, PASSWORD_WIFI);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(100);
+    digitalWrite(LED_BUILTIN, HIGH);
+    Serial.print(".");
+  }
+
+  Serial.println("Connected to WIFI: ");
+  Serial.println(WiFi.localIP());
+}
+
+void publishDataToDojot(float value) {
+  char payload[50];
+  char data[20];
+  dtostrf(value, 5, 0, data);
+  sprintf(payload, "{\"temperature\" : %s }", data);
+
+  mqqtClient.connect("nodemcu");
+  
+  if (mqqtClient.connected()) {
+    Serial.println("Sending temperature " + String(value, 0) + " °C to Dojot");
+    mqqtClient.publish(TOPIC_PUBLISH, payload);
+  } else {
+    Serial.println("MQTT Broker disconnected, data lost...");
+  }  
 }
 
 void drawProgressBar(int time) {
   int wait = (time / 100);
-
   for (int counter = 0; counter <= 100; counter++) {
     display.clear();
     display.setFont(ArialMT_Plain_16);
@@ -45,15 +92,19 @@ void checkLimitValue(int value, int maximum) {
 }
 
 void loop() {
+  wirelessConnection();
   drawProgressBar(dht.getMinimumSamplingPeriod());
-  
+
   float temperature = dht.getTemperature();
   float humidity = dht.getHumidity();
 
-  checkLimitValue(temperature, 30);
+  checkLimitValue(temperature, 26);
+  publishDataToDojot(temperature);
 
   drawSensorValue(temperature, " °C");
-  delay(10000);
+  delay(20000);
   drawSensorValue(humidity, " %");
   delay(5000);
+
+  mqqtClient.loop();
 }
